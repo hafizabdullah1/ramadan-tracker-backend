5c88c5ebb43051b8b6dc8de274fbd3ce1deaf147
hafiz abdullah <habdullah4510@gmail.com>
2025-03-05 00:04:37 +0500

    make some changes for deployment

--- ramadan_tracker/settings.py
+++ ramadan_tracker/settings.py
@@ -144,6 +144,9 @@ SIMPLE_JWT = {
 
 STATIC_URL = "static/"
 
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
